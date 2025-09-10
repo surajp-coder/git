03d7264ddc57833c1fe5db64e0143db2edfde686
surajp-coder <programmersuraj@gmail.com>
2025-09-10 22:37:53 +0530

    commit message

--- hsuraj.py
+++ hsuraj.py
@@ -1,2 +1,17 @@
 print("hello suraj ")
 print("how are you")
+# for the first time to config github in our system
+# git confi --global user.name "surajp-coder"
+# get config --global user.email "programmersuraj@gmail.com"
+# git config--global color.ui auto
+#mandatory command 
+#git Init
+#git remote add origin https://github.com/surajp-coder/git.git
+#git pull origin main
+# git status
+# git add .     git add file name
+# git commit -m "commit message"
+# git push 
+# git commit -m "initial commt"
+#create a new branch
+#git  checkout -b "branch name"
---
+++ notOnMain.py
@@ -0,0 +1,2 @@
+#--seperate branch:day1branch
+# git checkout -b "day1branch"
